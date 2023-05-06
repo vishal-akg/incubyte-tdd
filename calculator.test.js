429a0c69c95166d2584049b2aae921347c29d69f
Vishal Chaurasia <vishalchaurasiya02@gmail.com>
2023-05-06 15:10:48 +0530

    add test case for 1 and 2 input addition

--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,15 @@
 const { add } = require("./calulator");
 
 describe("Calulator", () => {
-  it("return 0 for empty string", () => {
+  it("returns 0 for empty string", () => {
     expect(add("")).toEqual(0);
+  });
+
+  it("returns the number itself if only one number is given", () => {
+    expect(add("1,")).toEqual(1);
+  });
+
+  it("returns 3 for the input 1,2", () => {
+    expect(add("1,2")).toEqual(3);
   });
 });
--- calulator.js
+++ calulator.js
@@ -1,6 +1,10 @@
 const add = (numbers) => {
   const numArray = numbers.split(",");
   if (numArray[0] === "") return 0;
+
+  if (numArray.length === 2 && numArray[1] === "") return parseInt(numArray[0]);
+
+  return parseInt(numArray[0]) + parseInt(numArray[1]);
 };
 
 module.exports = { add };
