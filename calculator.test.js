90b02cb7edce3b3bed059c5a2e46bd229a94e640
Vishal Chaurasia <vishalchaurasiya02@gmail.com>
2023-05-11 17:57:28 +0530

    final test

--- calculator.test.js
+++ calculator.test.js
@@ -1,31 +1,35 @@
-const { add } = require("./calulator");
+const { add } = require('./calulator');
 
-describe("Calulator", () => {
-  it("returns 0 for empty string", () => {
-    expect(add("")).toEqual(0);
+describe('Calulator', () => {
+  it('returns 0 for empty string', () => {
+    expect(add('')).toEqual(0);
   });
 
-  it("returns the number itself if only one number is given", () => {
-    expect(add("1,")).toEqual(1);
+  it('returns the number itself if only one number is given', () => {
+    expect(add('1,')).toEqual(1);
   });
 
-  it("returns 3 for the input 1,2", () => {
-    expect(add("1,2")).toEqual(3);
+  it('returns 3 for the input 1,2', () => {
+    expect(add('1,2')).toEqual(3);
   });
 
-  it("throws expection for unknown amount of numbers", () => {
-    expect(() => add("1,2,3,4,5")).toThrow(TypeError);
+  it('it returns the summation of all the numbers', () => {
+    expect(add('1,2,3,4,5')).toBe(15);
   });
 
-  it("handles new lines escape character", () => {
-    expect(add("1\n2,3")).toEqual(6);
+  it('handles new lines escape character', () => {
+    expect(add('1\n2,3')).toEqual(6);
   });
 
-  it("returns 3 for the given input with delimiter input", () => {
-    expect(add("//;\n1;2")).toEqual(3);
+  it('returns 3 for the given input with delimiter input', () => {
+    expect(add('//;\n1;2')).toEqual(3);
   });
 
-  it("throws exception for negative numbers", () => {
-    expect(() => add("-1,-2")).toThrow(TypeError);
+  it('throws exception for negative numbers', () => {
+    expect(() => add('-1,-2')).toThrow(TypeError);
+  });
+
+  it('should return 4 for * delimiter', () => {
+    expect(add('//*\n1*2*2')).toBe(4);
   });
 });
--- calulator.js
+++ calulator.js
@@ -1,32 +1,43 @@
 const add = (numbers) => {
   const delimiter = extractDelimiter(numbers);
 
-  if (numbers.indexOf("//") == 0) {
+  if (numbers.indexOf('//') == 0) {
     numbers = numbers.substring(4);
   }
 
-  if (numbers.split(delimiter).length > 2) {
-    throw new TypeError("Unknow amount of numbers");
+  if (numbers.indexOf(',\n') !== -1 || numbers.indexOf('\n,') !== -1) {
+    throw new TypeError('Invalid input sequences');
   }
 
-  if (numbers.indexOf(",\n") !== -1 || numbers.indexOf("\n,") !== -1) {
-    throw new TypeError("Invalid input sequences");
-  }
+  const numArray = numbers.replace('\n', ',').split(delimiter);
 
-  const numArray = numbers.replace("\n", ",").split(delimiter);
   const negatives = checkNegatives(numArray);
   if (negatives.length > 0) {
-    throw new TypeError(`negatives not allowed ${negatives.join(" ")}`);
+    throw new TypeError(`negatives not allowed ${negatives.join(' ')}`);
   }
 
-  if (numArray[0] === "") return 0;
+  if (numArray[0] === '') return 0;
 
-  if (numArray.length === 2 && numArray[1] === "") return parseInt(numArray[0]);
+  if (numArray.length === 2 && numArray[1] === '') return parseInt(numArray[0]);
 
-  return numArray.reduce((a, b) => {
-    return parseInt(a) + parseInt(b);
-  }, 0);
+  return performAction(numArray, delimiter);
 };
+
+/**
+ * perform mathmetical operation based on delimiter
+ *
+ */
+function performAction(numbers, operation) {
+  if (operation === '*') {
+    return numbers.reduce((aggregate, num) => {
+      return parseInt(num) * aggregate;
+    }, 1);
+  }
+
+  return numbers.reduce((aggregate, num) => {
+    return parseInt(num) + aggregate;
+  }, 0);
+}
 
 /**
  *
@@ -34,7 +45,7 @@ const add = (numbers) => {
  */
 
 function extractDelimiter(numbers) {
-  return numbers.indexOf("//") === 0 ? numbers[2] : ",";
+  return numbers.indexOf('//') === 0 ? numbers[2] : ',';
 }
 
 /**
